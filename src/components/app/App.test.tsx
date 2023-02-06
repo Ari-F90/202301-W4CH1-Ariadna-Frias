3de6bc08a62ef7994a7cef3eb5099f4e5b8c678d
Ariadna Frias <ariadna.812@gmail.com>
2023-02-06 21:35:12 +0100

    Add app test

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "../app/App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Given the App function", () => {
+  describe("When we call this function", () => {
+    test("renders learn react link", () => {
+      render(<App />);
+      const h1 = screen.getByText(/The pointing gentlemen/i);
+      expect(h1).toBeInTheDocument();
+    });
+  });
 });
