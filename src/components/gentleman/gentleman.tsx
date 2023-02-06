7c1da8e2ef38815929b39c2626054d50cb731d00
Ariadna Frias <ariadna.812@gmail.com>
2023-02-06 21:06:58 +0100

    Update gentleman tsx file

--- src/components/gentleman/gentleman.tsx
+++ src/components/gentleman/gentleman.tsx
@@ -9,27 +9,35 @@ export function Gentleman({
   alternativeText,
   selected,
 }: GentlemanType) {
-  <li className="gentleman">
-    <div className="gentleman__avatar-container">
-      <img className="gentleman__avatar" src={picture} alt={alternativeText} />
-      <span className="gentleman__initial">F</span>
-    </div>
-    <div className="gentleman__data-container">
-      <h2 className={name}>The Fary</h2>
-      <ul className="gentleman__data-list">
-        <li className="gentleman__data">
-          <span className="gentleman__data-label">Profession:</span>
-          Influencer
-        </li>
-        <li className="gentleman__data">
-          <span className="gentleman__data-label">Status:</span> RIP
-        </li>
-        <li className="gentleman__data">
-          <span className="gentleman__data-label">Twitter:</span> Pending
-        </li>
-      </ul>
-    </div>
-    <i className="icon gentleman__icon fas fa-check"></i>
-    <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
-  </li>;
+  return (
+    <li className="gentleman">
+      <div className="gentleman__avatar-container">
+        <img
+          className="gentleman__avatar"
+          src={"img/" + picture + ".jpeg"}
+          alt={alternativeText}
+        />
+        <span className="gentleman__initial">{id}</span>
+      </div>
+      <div className="gentleman__data-container">
+        <h2 className="gentleman__name">{name}</h2>
+        <ul className="gentleman__data-list">
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">{profession}</span>
+            Influencer
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Status:</span>
+            {status}
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Twitter:</span>
+            {twitter}
+          </li>
+        </ul>
+      </div>
+      <i className="icon gentleman__icon fas fa-check"></i>
+      <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+    </li>
+  );
 }
